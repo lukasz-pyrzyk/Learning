8f99c718d7e55a5dbc3792b09a5c0a333e0ce5ea
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-03 10:45:24 +0200

    Added uf in Java

---
+++ Algorithms/Part I/Week 1/uf.java
@@ -0,0 +1,44 @@
+public class uf 
+{
+    private int[] id;
+    private int[] sz;
+
+    public uf(int n)
+    {
+        id = new int[n];
+      	sz = new int[n];
+        for(int i = 0; i < n; i++) id[i] = i;
+    }
+
+    private int root(int i)
+    {
+        while(i != id[i])
+        {
+          	id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q)
+    {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q)
+    {
+        int i = root(p);
+        int j = root(q);
+      	if (i == j) return;
+      	if(sz[i] < sz[j])
+        {
+        	id[i] = j;
+          	sz[j] += sz[i];
+        }
+      	else
+        {
+        	id[j] = i;
+          	sz[i] += sz[j];
+        }
+    }
+}
