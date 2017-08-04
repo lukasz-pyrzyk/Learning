c15a809b6b6b54e608cf174df2b164dedc50d30e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-08-04 13:24:45 +0200

    Added requires files + two solutions

--- Programming languages/Racket/Week 1/hw4.rkt
+++ Programming languages/Racket/Week 1/hw4.rkt
@@ -5,3 +5,12 @@
 
 ;; put your code below
 
+; AD 1
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
+
+; AD 2
+(define (string-append-map xs suffix)
+  (map (lambda (i) (string-append i suffix)) xs))
