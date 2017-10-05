aa71842d9684234aa5c230cddac6018b2be0286c
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-10-05 21:41:17 +0200

    Added first point of homework

--- Programming languages/Ruby/Week 1/hw6assignment.rb
+++ Programming languages/Ruby/Week 1/hw6assignment.rb
@@ -16,7 +16,39 @@ class MyBoard < Board
 end
 
 class MyTetris < Tetris
-  # your enhancements here
+  def initialize
+    @root = TetrisRoot.new
+    @timer = TetrisTimer.new
+    set_board
+    @running = true
+    key_bindings
+    buttons
+    run_game
+  end
+
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = Board.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def run_game
+    if !@board.game_over? and @running
+      @timer.stop
+      @timer.start(@board.delay, (proc{@board.run; run_game}))
+    end
+  end
+
+  def key_bindings
+    @root.bind('u', proc {rotate})
+  end
+
+  def rotate
+    @board.rotate_clockwise
+    @board.rotate_clockwise
+  end
 
 end
 
