e1c0835b82c66a88d2cbcd744c537ed30476356e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-11-03 21:34:29 +0100

    Exports

---
+++ Programming languages/TypeScript/Module 3/imports.ts
@@ -0,0 +1,7 @@
+import { ZipCodeValidator } from "./ZipCodeValidator";
+
+let myValidator = new ZipCodeValidator();
+
+
+import { ZipCodeValidator as ZCV } from "./ZipCodeValidator";
+let myRenamedValidator = new ZCV();
