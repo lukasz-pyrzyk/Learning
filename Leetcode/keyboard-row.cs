fd46d2053897476e96eea426b5e71195564b3ddf
Lukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-11-09 17:40:35 +0100

    Changes for keyboard row

--- Leetcode/keyboard-row.cs
+++ Leetcode/keyboard-row.cs
@@ -1,13 +1,16 @@
 public class Solution {
     public string[] FindWords(string[] words) {
-        var firstLine = new HashSet<char>(new[] {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'});
-        var second = new HashSet<char>(new[] {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'});
-        var third = new HashSet<char>(new[] {'z', 'x', 'c', 'd', 'v',' b', 'n', 'm'});
+        var rows = new HashSet<char>[] {
+            new HashSet<char>(new[] {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}),
+            new HashSet<char>(new[] {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'}),
+            new HashSet<char>(new[] {'z', 'x', 'c', 'b', 'v', 'b', 'n', 'm'})
+        };
         
         var results = new List<string>();
         foreach(var word in words)
         {
-            if(word.All(x => firstLine.Contains(x) || second.All(x => firstLine.Contains(x) || word.All(x => third.Contains(x))
+            char[] chars = word.ToCharArray();
+            if(rows.Any(r => chars.All(c => r.Contains(Char.ToLower(c)))))
             {
                 results.Add(word);
             }
