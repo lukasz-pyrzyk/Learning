e7b3e3afd9104df836a778c976815cf989d39974
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-11-12 16:14:36 +0100

    Usage of the external libs

--- Programming languages/TypeScript/Module 3/namespacesAndModulesLabs/externalModulesLab/main.ts
+++ Programming languages/TypeScript/Module 3/namespacesAndModulesLabs/externalModulesLab/main.ts
@@ -1,4 +1,12 @@
 import ArrayUtilities from './arrayUtils.js'
+
+declare namespace zepto {
+    export interface Base {
+        each: Function
+    }
+}
+declare var $: zepto.Base;
+
 let array1 = [
     1,
     2,
@@ -14,11 +22,11 @@ let array2 = [
     10
 ];
 function callArrayUtil(util, array, array2 = null) {
-    document.getElementById("output").innerHTML = ArrayUtilities[util](array, array2).toString()
+    document.getElementById('output').innerHTML = ArrayUtilities[util](array, array2).toString()
 }
-for (let property in ArrayUtilities) {
+$.each(ArrayUtilities, function(property, index) {
     document.getElementById(property).onclick = function() {
         callArrayUtil(property, array1, array2)
     }
-}
-document.getElementById("output").innerHTML= array1.toString();
+})
+document.getElementById("output").innerHTML = array1.toString();
