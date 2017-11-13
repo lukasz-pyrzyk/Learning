090df9745008196a5cc6e8267f1566d82e8e17a6
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-11-13 16:37:39 +0100

    Added generics

---
+++ Programming languages/TypeScript/Module 4/generics.ts
@@ -0,0 +1,9 @@
+function identity(arg: number): number {
+    return arg;
+}
+function identityAsAny(arg: any): any {
+    return arg;
+}
+function identityGeneric<T>(arg: T): T {
+    return arg;
+}
